4d85809d12fc1ddf3a2daf791b4fe5c826108d8f
hel1yeah <hel1_yeah@ukr.net>
2021-09-14 21:34:14 +0300

    fix: use global fonts

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount("#app");
+import './assets/scss/main.scss';
+
+const app = createApp(App);
+
+app.mount('#app');
