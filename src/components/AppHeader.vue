90ef3740eee12adab82c35c792bf30746cdb14a2
hel1yeah <hel1_yeah@ukr.net>
2021-09-14 22:53:41 +0300

    fix: bugs with sass-loader, daungrade sass-loader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <div class="header__cleaning">
+      <img class="header__logo" src="" alt="" />
+      <div class="header__swith"></div>
+    </div>
+    <nav class="header__nav">
+      <ul class="header__nav-list">
+        <li class="header__nav-list-item">После ремонта</li>
+        <li class="header__nav-list-item">Генеральная уборка</li>
+        <li class="header__nav-list-item">Регулярная уборка</li>
+        <li class="header__nav-list-item">Мойка окон</li>
+      </ul>
+    </nav>
+    <div class="header__contacts">
+      <a class="header__contacts-link" href="tel:+380 67 401 69 77">
+        +380 67 401 69 77</a
+      >
+      <span class="header__contacts-support">27/7</span>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,4 @@ import App from './App.vue';
 
 import './assets/scss/main.scss';
 
-const app = createApp(App);
-
-app.mount('#app');
+createApp(App).mount('#app');
